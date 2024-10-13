8463f493f0b7bc639ee56f517d71ebb3002ecd3e
David Milet <david.milet@gmail.com>
2024-10-13 18:14:20 -0400

    new version of container app support for POST

--- apps/frontends/flaskapp/src/main.py
+++ apps/frontends/flaskapp/src/main.py
@@ -1,7 +1,16 @@
 from flask import Flask
+from flask import Response
+from flask import request
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
+@app.route("/", methods = ['GET'])
+def get_hello_world():
     return "<p>Hello, Brave New World!</p>"
+
+
+@app.route("/", methods = ['POST'])
+def post_hello_world():
+    events = request.form
+    print (f"{events}")
+    return Response(events, status=201, mimetype='application/json')
